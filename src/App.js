dc0c95bbe32984c42468d15434c7683338827f6c
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-02-02 18:24:13 +0800

    created navbar and homepage component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Link, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import UserProfilePage from "./pages/UserProfilePage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ height: "100vh", width: "100vw", padding: "0", margin: "0" }}
+    >
+      <Navbar />
+
+      {/* USING REACT ROUTER TO LINK TO SPECIFIC PAGES */}
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/users/1">My Profile</Link>
+
+        <Route exact path="/" component={HomePage} />
+        <Route path="/users/:id" component={UserProfilePage} />
+      </div>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="landing-page">
+      <h1 style={{ marginTop: "0" }}>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
